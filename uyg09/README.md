3dbe531ea1394a52d45383e872b0bf45cbbab545
akocer <akocerweb@hotmail.com>
2022-11-10 16:12:25 +0300

    Uygulama 9

--- uyg09/README.md
+++ uyg09/README.md
@@ -6,6 +6,10 @@
 
 <a href="https://github.com/typicode/json-server" target="_blank">JSON-Server Github Sayfası</a>
 
+`ng add @ngneat/hot-toast` komutu ile Hot Toast kurulur
+
+<a href="https://github.com/ngneat/hot-toast" target="_blank">Hot Toast Github Sayfası</a>
+
 ![alt text](https://github.com/akocer/Internet-1/blob/main/uyg09/1.png?raw=true)
 ![alt text](https://github.com/akocer/Internet-1/blob/main/uyg09/2.png?raw=true)
 ![alt text](https://github.com/akocer/Internet-1/blob/main/uyg09/3.png?raw=true)
--- uyg09/src/app/services/auth.guard.ts
+++ uyg09/src/app/services/auth.guard.ts
@@ -1,12 +1,18 @@
+import { DataService } from 'src/app/services/data.service';
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
 
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(
+    public servis: DataService
+  ) {
+
+  }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     var sonuc: boolean = false;
-    if (localStorage.getItem("adsoyad")) {
+    if (this.servis.OturumKontrol()) {
       sonuc = true;
     } else {
       location.href = "/login";
