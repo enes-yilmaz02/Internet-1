b5f4b64eb3cc4e75ebeb84d580fee5c5b9aa3ca1
akocer <akocerweb@hotmail.com>
2022-11-28 16:51:48 +0300

    Uygulama !0

---
+++ uyg10/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { DersComponent } from './components/ders/ders.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './services/auth.guard';
+import { MytoastService } from './services/mytoast.service';
+import { DataService } from 'src/app/services/data.service';
+import { UyeComponent } from './components/uye/uye.component';
+import { KategoriComponent } from './components/kategori/kategori.component';
+import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HotToastModule } from '@ngneat/hot-toast';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    KategoriComponent,
+    UyeComponent,
+    LoginComponent,
+    DersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    HotToastModule.forRoot(),
+  ],
+  providers: [DataService, MytoastService, AuthGuard],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
